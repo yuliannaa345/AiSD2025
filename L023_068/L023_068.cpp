33b3e83eb0121bd0b6dc1fb5ec98ca780b1cb86d
vjacheslavparf05-glitch <vjacheslav.parf05@gmail.com>
2025-09-23 01:12:18 +0500

    Поиск строки в тексте

---
+++ L023_068/L023_068.cpp
@@ -0,0 +1,29 @@
+﻿//L023_068
+//Поиск строки в тексте
+//ПМ-2025 Парфёнов Вячеслав 23.09.2025
+#include <iostream>
+#include <cstring>
+#include "L023_068.h"
+using namespace std;
+
+int findmatch(char* p, char* t) {
+    int i, j;
+    int plen, tlen;
+
+    plen = strlen(p);
+    tlen = strlen(t);
+
+    // Основной цикл по всем возможным позициям начала образца в тексте
+    for (i = 0; i <= (tlen - plen); i = i + 1) {
+        j = 0;
+        // Проверка совпадения символов образца и текста
+        while ((j < plen) && (t[i + j] == p[j])) {
+            j = j + 1;
+        }
+        // Если весь образец совпал с частью текста
+        if (j == plen) {
+            return i;
+        }
+    }
+    return -1;
+}
---
+++ L023_068/L023_068.h
@@ -0,0 +1,4 @@
+#ifndef L023_068_H
+#define L023_068_H
+int findmatch(char* p, char* t);
+#endif
