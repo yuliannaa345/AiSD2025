2ddf76e3e5b8ae797adc6423cc255d43780dd4f9
vjacheslavparf05-glitch <vjacheslav.parf05@gmail.com>
2025-09-29 22:03:18 +0500

    Быстрое возведение в степень

---
+++ L025_077/L025_077test.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "L025_077.hpp"
+using namespace std;
+
+int main() {
+    system("chcp 65001 > nul");
+
+    int base, exponent;
+
+    cout << "Введите основание: ";
+    cin >> base;
+
+    cout << "Введите показатель степени: ";
+    cin >> exponent;
+
+    long long result = power(base, exponent);
+
+    cout << base << " ^ " << exponent << " = " << result << endl;
+
+    return 0;
+}
