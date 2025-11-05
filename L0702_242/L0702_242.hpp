3a31fd81c9c7d8279e1eb59af73ef2233fb0dade
kopicentr <vitalijkarass@gmail.com>
2025-11-06 02:10:07 +0500

    L0702_242

---
+++ L0702_242/L0702_242.hpp
@@ -0,0 +1,26 @@
+#ifndef L0702_242_HPP
+#define L0702_242_HPP
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MAXV 100
+
+struct EdgeNode {
+int y;
+int weight;
+struct EdgeNode *next;
+};
+
+
+struct Graph {
+vector<EdgeNode*> edges;
+vector<int> degree;
+int nvertices;
+int nedges;
+int directed;
+};
+
+void initialize_graph(Graph &g, bool directed);
+
+#endif
---
+++ L0702_242/L0702_242_test.cpp
@@ -0,0 +1,16 @@
+//Карасс Виталий, МХ-201,Инициализация графа, L0702_242
+
+#include <iostream>
+#include "L0702_242.hpp"
+using namespace std;
+
+#define MAXV 100
+
+
+
+int main() {
+Graph graph; 
+initialize_graph (graph, 0);
+
+ return 0;
+}
