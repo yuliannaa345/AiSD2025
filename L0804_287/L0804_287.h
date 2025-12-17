ad4ed3158b840bc924f73c6c1a4586069a5bb932
nastyaa-ivanovaa <nastya_ivanovaa06@mail.ru>
2025-12-17 16:44:16 +0500

    Commit

--- L0804_287/L0804_287.h
+++ L0804_287/L0804_287.h
@@ -3,22 +3,22 @@
 
 #include <iostream>
 
-const int MAXV = 100;            // Максимальное количество вершин в графе
+const int MAXV = 100;
 const int MAXINT = 1000000;
 
 // Структура ребра
 struct edgenode {
-    int y;                       // Номер вершины, в которую ведет это ребро
+    int y;                   
     int weight;
     edgenode* next;
 };
 
 // Структура графа
 struct graph {
-    edgenode* edges[MAXV+1];    // Массив списков смежности
-    int nvertices;              // Количество вершин
-    int nedges;                 // Количество ребер
-    bool directed;              // Ориентированный ли граф
+    edgenode* edges[MAXV+1];    
+    int nvertices;             
+    int nedges;                 
+    bool directed;            
 };
 
 void initialize_graph(graph* g, bool directed);
